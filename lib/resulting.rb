e9ea4c7585b819dfda2b3b1029e6f3bfaa067c32
John DeWyze <john@dewyze.dev>
2020-04-14 11:38:14 -0500

    Add resulting call method
    This method will take a result or object, if it is a failing result,
    return early, or otherwise run the block. If the block is successful, it
    will return a success result, otherwise it will return a failure result.
    
    It will also ensure that it returns the klass of result that is passed
    into it if it's not a default result object.

--- lib/resulting.rb
+++ lib/resulting.rb
@@ -5,6 +5,22 @@ require "resulting/resultable"
 require "resulting/result"
 
 module Resulting
+  def self.call(result_or_value)
+    if result_or_value.is_a?(Resulting::Resultable)
+      return result_or_value if result_or_value.failure?
+
+      value =  result_or_value.value
+      klass =  result_or_value.class
+    else
+      value =  result_or_value
+      klass =  Resulting::Result
+    end
+
+    success = yield
+
+    klass.new(success, value)
+  end
+
   class << self
     attr_writer :configuration
 
--- spec/resulting_spec.rb
+++ spec/resulting_spec.rb
@@ -3,6 +3,70 @@ RSpec.describe Resulting do
     expect(Resulting::VERSION).to_not be nil
   end
 
+  describe ".call" do
+    subject(:run) { described_class.call(result, &blk) }
+
+    let(:test_class) do
+      Class.new do
+        include Resulting::Resultable
+      end
+    end
+    let(:object) { Object.new }
+    let(:result) { test_class.new(success, object) }
+
+    let(:blk) { -> { block_success } }
+
+    context "with a failing result" do
+      let(:success) { false }
+
+      it "returns a failure" do
+        result = run
+
+        expect(result).to be_failure
+        expect(result.value).to eq(object)
+      end
+
+      it "does not call the block" do
+        expect { |blk| described_class.call(result, &blk) }.to_not yield_control
+      end
+    end
+
+    context "with a successful result" do
+      let(:success) { true }
+      let(:block_success) { [true, false].sample }
+
+      it "returns a success with the same class" do
+        result = run
+
+        expect(result.success?).to eq(block_success)
+        expect(result.value).to eq(object)
+        expect(result.class).to eq(test_class)
+      end
+
+      it "does call the block" do
+        expect { |blk| described_class.call(result, &blk) }.to yield_control
+      end
+    end
+
+    context "with an object" do
+      let(:result) { object }
+      let(:success) { true }
+      let(:block_success) { [true, false].sample }
+
+      it "returns a success" do
+        result = run
+
+        expect(result.success?).to eq(block_success)
+        expect(result.value).to eq(object)
+        expect(result.class).to eq(Resulting::Result)
+      end
+
+      it "does call the block" do
+        expect { |blk| described_class.call(result, &blk) }.to yield_control
+      end
+    end
+  end
+
   describe ".configuration" do
     it "returns a new configuration if not yet defined" do
       expect(described_class.configuration).to be_a(Resulting::Configuration)
